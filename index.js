eaa76c3102c9caa6e3286048b2272f5dcb122eb5
Luis Rafael Santiago Villalta <santiagovillalta@outlook.com>
2018-10-13 18:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string){
-console.log ('hello'.toUpperCase)
+console.log (string.toUpperCase)
 }
 function logWhisper (string){
-console.log ('HELLO'.toLowerCase)
+console.log (string.toLowerCase)
 }
