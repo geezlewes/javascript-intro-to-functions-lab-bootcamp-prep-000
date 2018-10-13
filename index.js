2a59b1647308296d8ec3ab35bddf5fea605c9510
Luis Rafael Santiago Villalta <santiagovillalta@outlook.com>
2018-10-13 18:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,4 @@ console.log (string.toUpperCase)
 function logWhisper (string){
 console.log (string.toLowerCase)
 }
-function sayHitoGrandma(string){
- returns string.toLowerCase
- returns string.toUpperCase
- if sayHitoGrandma(string)=== "I love you too"{
- return "I love you too, Grandma."
- }
- }
+
